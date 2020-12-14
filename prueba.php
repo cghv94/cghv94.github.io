6f443847e47c1b98344409f35d683cce7afac028
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2020-12-14 10:00:11 -0600

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center>
+
+<form action="guardar.php" method="post" name="frm">
+<input type="text" name="nombre" /><br/>
+<input type="text" name="contraseña" /><br/>
+<input type="submit" value="Iniciar sesión" />
+</form>
+</center>
+
+</body>
+</html>
