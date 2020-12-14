ae2e8f5132fbe7a31633a764ec4c83cccf4f6fcf
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2020-12-14 10:16:24 -0600

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -4,8 +4,10 @@
 
 <center>
 
-<form action="guardar.php" method="post" name="frm">
+<form action="guardar.php" method="post" name="frm"><br/>
+<b>Correo electrónico<b/><br/>
 <input type="text" name="nombre" /><br/>
+<b>Contraseña<b/><br/>
 <input type="text" name="contraseña" /><br/>
 <input type="submit" value="Iniciar sesión" />
 </form>
